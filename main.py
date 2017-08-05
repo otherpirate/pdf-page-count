8368ce3e643b6be177b9811bab9884f722378f85
otherpirate <mauro_murari@hotmail.com>
2017-08-05 14:15:55 -0300

    Dealing with all files in folder

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
-from os import path, rename
+from os import listdir, path, rename
 from PyPDF2 import PdfFileReader
+from PyPDF2.utils import PdfReadError
 
 
 def main():
-    pdf_path = input('Path: ')
-    reader = PdfFileReader(pdf_path)
-    number_of_pages = str(reader.numPages)
-    print('Pages: {}'.format(number_of_pages))
+    pdf_folder = input('Folder: ')
+    files = listdir(pdf_folder)
+    for filename in files:
+        full_path = path.join(pdf_folder, filename)
 
-    base_path, file_name = path.split(pdf_path)
-    if not file_name.startswith(number_of_pages):
-        new_file_name = '{}-{}'.format(number_of_pages, file_name)
-        new_pdf_path = '{}/{}'.format(base_path, new_file_name)
-        print('Renaming file {} to {}'.format(file_name, new_file_name))
-        rename(pdf_path, new_pdf_path)
+        try:
+            reader = PdfFileReader(full_path)
+        except PdfReadError:
+            print('Could not open {}'.format(filename))
+            continue
+
+        number_of_pages = str(reader.numPages)
+        print('{}: {}'.format(filename, number_of_pages))
+
+        if not filename.startswith(number_of_pages):
+                new_file_name = '{}-{}'.format(number_of_pages, filename)
+                print('Renaming file {} to {}'.format(filename, new_file_name))
+                new_pdf_path = '{}/{}'.format(pdf_folder, new_file_name)
+                rename(full_path, new_pdf_path)
 
 
 if __name__ == '__main__':
